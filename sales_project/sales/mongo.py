41da99f24868ee034a221c674017c1f67239d65d
ikuta-mako <ikuta.mako.24@shizuoka.ac.jp>
2026-01-26 14:08:00 +0900

    Update mongo.py

--- sales_project/sales/mongo.py
+++ sales_project/sales/mongo.py
@@ -1,12 +1,13 @@
-import os
+from django.conf import settings
 from pymongo import MongoClient
 
+# settings.py で MONGO_URI と MONGO_DB_NAME を定義しておく前提
 client = MongoClient(settings.MONGO_URI)
-db = client[settings.MONGO_DB_NAME]
-
+db = client[settings.MONGO_DB]
 
 tenpo_col = db["tenpo"]
 uriage_col = db["uriage"]
+
 
 def get_sales_data():
     tenpos = list(tenpo_col.find({}, {"_id": 0}))
@@ -15,15 +16,13 @@ def get_sales_data():
     tenpo_dict = {t["tenpo_id"]: t["name"] for t in tenpos}
 
     result = []
-
     for u in uriages:
-         result.append({
+        result.append({
             "tenpo_id": u["tenpo_id"],
             "tenpo_name": tenpo_dict.get(u["tenpo_id"], "不明"),
             "date": u["date"],
             "amount": u["amount"],
         })
-
 
     return result
 
